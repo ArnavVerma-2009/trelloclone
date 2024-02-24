ac9af30af93f945da66f97ea5904d90f2981ae52
ARNAVVERMAbang <arnav.verma.feb.2009@gmail.com>
2024-02-24 23:54:24 +0530

    fixed database for production

--- app/(dashboard)/[userId]/page.tsx
+++ app/(dashboard)/[userId]/page.tsx
@@ -1,5 +1,6 @@
+import { Input } from "@/components/ui/input";
 import { CurrentProfile } from "@/lib/current-profile";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, currentUser } from "@clerk/nextjs";
 
 interface DashBoardLayoutProps {
   params: {
@@ -7,12 +8,17 @@ interface DashBoardLayoutProps {
   };
 }
 const DashBoardLayout = async ({ params }: DashBoardLayoutProps) => {
-  const user = await CurrentProfile();
+  const user = await currentUser();
   return (
     <div className="bg-neutral-900  text-white h-full">
       <div className="text-4xl h-20 flex justify-between border-b border-[#ffffff25] w-full items-center pl-2">
         Taskify
-        
+        <div className="w-full px-8">
+          <Input
+            className="bg-neutral-800  border-[#ffffff25] ring-0 border active:ring-0 w-full"
+            placeholder="Search your boards here"
+          />
+        </div>
         <UserButton
           afterSignOutUrl="/"
           appearance={{
@@ -27,6 +33,15 @@ const DashBoardLayout = async ({ params }: DashBoardLayoutProps) => {
             },
           }}
         />
+      </div>
+
+      <div>
+        <div className="text-3xl font-semibold pt-4 pl-4">
+          Hi {user?.firstName} !
+        </div>
+        <div className="text-[#ffffff50] pl-4 -mt-1">
+          optimize and increase your productivity by using Taskify
+        </div>
       </div>
     </div>
   );

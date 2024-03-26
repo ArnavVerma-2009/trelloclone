3f09bf97da548a2238260dd017e564409a13c47e
ARNAVVERMAbang <arnav.verma.feb.2009@gmail.com>
2024-03-26 17:38:02 +0530

    Redirect fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import { Button } from "@/components/ui/button";
 import { initialProfile } from "@/lib/create-profile";
 import { db } from "@/lib/db";
-import { UserButton, redirectToSignIn } from "@clerk/nextjs";
+import {
+  UserButton,
+  auth,
+  redirectToSignIn,
+  redirectToSignUp,
+} from "@clerk/nextjs";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
   const user = await initialProfile();
-    return redirect(`${user?.id}`)
+
+  if (!user) {
+    return redirect("/sign-in");
+  }
+  return redirect(`${user?.id}`);
 }
